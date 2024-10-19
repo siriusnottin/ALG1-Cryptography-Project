c5111d73ac973fb050390ce50957d9594e2b5fab
Sirius Nottin <30043962+siriusnottin@users.noreply.github.com>
2024-10-19 16:33:28 +0200

    Initial Commit

---
+++ crypt.py
@@ -0,0 +1,72 @@
+# def lire(nom_fichier):
+
+
+# def ecrire(nom_fichier, texte):
+
+
+# def minuscules(texte):
+
+
+# def shift_enc(plain_text, key):
+
+
+# def shift_dec(cipher_text, key):
+
+
+# def compter_lettres_minuscules(texte):
+
+
+# def compter_lettres_majuscules(texte):
+
+
+# def max_lettres(freq_lettres):
+
+
+# def chaine_en_mots(texte):
+
+
+# def compter_mots(mots):
+
+
+# max_mot_1(freq_mots):
+
+
+# def francais(texte):
+
+
+# def break_shift(cipher_text):
+
+
+# def lire_clef_subst(nom_fichier):
+
+
+# def ecrire_clef_subst(nom_fichier, clef):
+
+
+# def subst_enc(plain_text, key):
+
+
+# def subst_dec_key(key):
+
+
+# def subst_dec(cipher_text, key):
+
+
+#def break_subst(cipher_text):
+
+
+# def lire_clef_poly(nom_fichier):
+
+
+# def ecrire_clef_poly(nom_fichier, clef):
+
+
+# def poly_enc(plain_text, key):
+
+
+# def poly_dec_key(key):
+
+
+# def poly_dec(cipher_text, key):
+
+
