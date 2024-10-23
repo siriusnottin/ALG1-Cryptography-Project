215f34a7e57f57cd090f755016f9a9935e5bfae4
Sirius Nottin <30043962+siriusnottin@users.noreply.github.com>
2024-10-23 17:04:00 +0200

    refactor: misc

--- src/encryption_methods/shift.py
+++ src/encryption_methods/shift.py
@@ -1,14 +1,20 @@
 # Author: Alex
 def shift_enc(plain_text, key, log):
-    # we shift each letter by key number
+    # a:65 z:90
+    # A:65 B:90
     log.info(f"Shifting text by {key} character…")
+    plain_text_shifted = ""
     for letter in plain_text:
         letter_shifted = chr(ord(letter) + key).upper()
+        plain_text_shifted += letter_shifted
         log.debug(f"shifting '{letter}' to '{letter_shifted}'")
+    return plain_text_shifted
 
 
 # Author: Sirius
 def shift_dec(cipher_text, key, log):
+    # a:65 z:90
+    # A:65 B:90
     pass
 
 
