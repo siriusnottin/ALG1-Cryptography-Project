793e82d274eb4d2eabc5539a102a605a254ee412
Sirius Nottin <30043962+siriusnottin@users.noreply.github.com>
2024-10-23 16:58:22 +0200

    refactor: test_shift.py

--- src/encryption_methods/test_shift.py
+++ src/encryption_methods/test_shift.py
@@ -1,4 +1,4 @@
-from .shift import shift_enc
+from .shift import shift_dec, shift_enc
 
 
 class MockLogger:
@@ -14,12 +14,49 @@ class MockLogger:
 
 def test_shift_enc():
     mock_log = MockLogger()
-    shift_enc("abc", 1, mock_log)
+    result = shift_enc("abc", 1, mock_log)
+    assert result == "BCD"
     assert mock_log.messages == [
         "Shifting text by 1 character…",
         "shifting 'a' to 'B'",
         "shifting 'b' to 'C'",
         "shifting 'c' to 'D'",
+    ]
+
+
+def test_shift_enc_with_negative_key():
+    mock_log = MockLogger()
+    result = shift_enc("abc", -1, mock_log)
+    assert result == "ZAB"
+    assert mock_log.messages == [
+        "Shifting text by -1 character…",
+        "shifting 'a' to 'Z'",
+        "shifting 'b' to 'A'",
+        "shifting 'c' to 'B'",
+    ]
+
+
+def test_shift_dec():
+    mock_log = MockLogger()
+    result = shift_dec("BCD", 1, mock_log)
+    assert result == "ABC"
+    assert mock_log.messages == [
+        "Shifting text by -1 character…",
+        "shifting 'B' to 'A'",
+        "shifting 'C' to 'B'",
+        "shifting 'D' to 'C'",
+    ]
+
+
+def test_shift_dec_with_negative_key():
+    mock_log = MockLogger()
+    result = shift_dec("ZAB", -1, mock_log)
+    assert result == "ABC"
+    assert mock_log.messages == [
+        "Shifting text by 1 character…",
+        "shifting 'Z' to 'A'",
+        "shifting 'A' to 'B'",
+        "shifting 'B' to 'C'",
     ]
 
 
